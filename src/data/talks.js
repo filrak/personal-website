44a4a41b8470108b5a017afc1bdd1a1323f8a18e
Filip Rakowski <filiprakowski@Filips-MacBook-Pro.local>
2023-01-16 18:35:21 +0100

    add talks

--- src/data/talks.js
+++ src/data/talks.js
@@ -2,15 +2,15 @@ const talks = [{
     title: 'How we ',
     date: '2023-05-12',
     readable_date: 'May 12 - 15 \'23',
-    event: 'Vue.js Live',
-    link: '',
+    event: 'You’re probably using Lighthouse wrong - how it works and how to interpret the results corectly.',
+    link: 'https://vuejslive.com/',
     location: 'UK'
 }, {
-    title: 'Secret Announcement!',
+    title: "You’re probably using Lighthouse wrong - how it works and how to interpret the results corectly.",
     date: '2023-02-09',
     readable_date: 'Feb 9 - 10 \'23',
     event: 'Vue.js Amsterdam',
-    link: '',
+    link: 'https://vuejs.amsterdam/',
     location: 'NL'
 }]
 
