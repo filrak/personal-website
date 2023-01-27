ca5c836355cbd53ba1913ff92225d5dae79aee3f
Filip <f.rakowski@hotmail.com>
2023-01-27 19:12:27 +0100

    fix footer links, remove btns from home

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,6 +1,6 @@
 <template>
     <footer class="flex bg-primary-lighter w-full">
-        <div class="container mx-auto flex p-5 ">
+        <div class="container mx-auto p-5">
         Built with <a href="https://vuejs.org">Vue.js</a> & <a href="https://astro.build">Astro</a>. Deployed to <a href="https://netlify.com">Netlify</a>.
         </div>
     </footer>
