90db111be1c897d9e46193ef3126cfb44c25cb7e
Filip <f.rakowski@hotmail.com>
2024-11-04 13:44:36 +0100

    fix footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,7 +1,7 @@
 <template>
-    <footer class="flex bg-primary-lighter w-full">
-        <div class="container mx-auto p-5">
-        Built with <a href="https://vuejs.org">Vue.js</a> & <a href="https://astro.build">Astro</a>. Deployed to <a href="https://netlify.com">Netlify</a>.
+    <footer class="bg-primary-lighter w-full">
+        <div class="container mx-auto px-5 md:px-0 py-5">
+            Built with <a href="https://vuejs.org">Vue.js</a> & <a href="https://astro.build">Astro</a>. Deployed to <a href="https://netlify.com">Netlify</a>.
         </div>
     </footer>
 </template>
